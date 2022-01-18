3bac5bb2eabcb87daf025426e7b2bcc426e41492
xAlessandroC <alexcalvio@hotmail.it>
2022-01-18 11:17:25 +0100

    Add instantiation messages management

--- src/nodes/mec/MECPlatformManager/Dynamic/MecPlatformManagerDyn.cc
+++ src/nodes/mec/MECPlatformManager/Dynamic/MecPlatformManagerDyn.cc
@@ -9,6 +9,8 @@
 #include <sstream>
 
 #include "nodes/mec/MECPlatformManager/Dynamic/MecPlatformManagerDyn.h"
+#include "nodes/mec/MECOrchestrator/MECOMessages/MECOrchestratorMessages_m.h"
+#include "inet/networklayer/common/L3AddressTag_m.h"
 
 Define_Module(MecPlatformManagerDyn);
 
@@ -58,17 +60,61 @@ void MecPlatformManagerDyn::handleMessage(cMessage *msg)
 {
     EV << "MecPlatformManagerDyn::handleMessage - message received - " << msg << endl;
 
+    if (msg->isSelfMessage()){
+        EV << "MecPlatformManagerDyn::handleMessage - self message received - " << msg << endl;
+
+    }else{
+        EV << "MecPlatformManagerDyn::handleMessage - other message received - " << msg << endl;
+
+        inet::Packet* pPacket = check_and_cast<inet::Packet*>(msg);
+        if (pPacket == 0)
+            throw cRuntimeError("MecPlatformManagerDyn::handleMessage - FATAL! Error when casting to inet packet");
+
+        if (!strcmp(msg->getName(), "Instantiation")){
+            std::cout << "Richiesta instanziazione ricevuta" << endl;
+            auto data = pPacket->peekData<CreateAppMessage>();
+            inet::PacketPrinter printer;
+            printer.printPacket(std::cout, pPacket);
+
+            // Ricostruzione pacchetto TODO cambiare
+            CreateAppMessage * createAppMsg = new CreateAppMessage();
+            createAppMsg->setUeAppID(data->getUeAppID());
+            createAppMsg->setMEModuleName(data->getMEModuleName());
+            createAppMsg->setMEModuleType(data->getMEModuleType());
+
+            createAppMsg->setRequiredCpu(data->getRequiredCpu());
+            createAppMsg->setRequiredRam(data->getRequiredRam());
+            createAppMsg->setRequiredDisk(data->getRequiredDisk());
+
+            createAppMsg->setRequiredService(data->getRequiredService());
+            createAppMsg->setContextId(data->getContextId());
+
+            instantiateMEApp(createAppMsg);
+
+            getParentModule()->bubble("Richiesta di instanziazione ricevuta");
+        }else{
+            EV << "MecPlatformManagerDyn::handleMessage - unknown package" << endl;
+        }
+
+//        std::cout << "IP received " << pPacket->getTag<inet::L3AddressInd>()->getSrcAddress() << endl;
+//        std::cout << "NAME received " << msg->getName() << endl;
+
+        delete msg;
+    }
+
 }
 
 MecAppInstanceInfo* MecPlatformManagerDyn::instantiateMEApp(CreateAppMessage* msg)
 {
-   MecAppInstanceInfo* res = vim->instantiateMEApp(msg);
-   delete msg;
-   return res;
+    EV << "MecPlatformManagerDyn::instantiateMEApp" << endl;
+    MecAppInstanceInfo* res = vim->instantiateMEApp(msg);
+    delete msg;
+    return res;
 }
 
 bool MecPlatformManagerDyn::instantiateEmulatedMEApp(CreateAppMessage* msg)
 {
+    EV << "MecPlatformManagerDyn::instantiateEmulatedMEApp" << endl;
     bool res = vim->instantiateEmulatedMEApp(msg);
     delete msg;
     return res;
@@ -76,6 +122,7 @@ bool MecPlatformManagerDyn::instantiateEmulatedMEApp(CreateAppMessage* msg)
 
 bool MecPlatformManagerDyn::terminateEmulatedMEApp(DeleteAppMessage* msg)
 {
+    EV << "MecPlatformManagerDyn::terminateEmulatedMEApp" << endl;
     bool res = vim->terminateEmulatedMEApp(msg);
     delete msg;
     return res;
@@ -84,6 +131,7 @@ bool MecPlatformManagerDyn::terminateEmulatedMEApp(DeleteAppMessage* msg)
 
 bool MecPlatformManagerDyn::terminateMEApp(DeleteAppMessage* msg)
 {
+    EV << "MecPlatformManagerDyn::terminateMEApp" << endl;
     bool res = vim->terminateMEApp(msg);
     delete msg;
     return res;
@@ -91,6 +139,7 @@ bool MecPlatformManagerDyn::terminateMEApp(DeleteAppMessage* msg)
 
 const std::vector<ServiceInfo>* MecPlatformManagerDyn::getAvailableMecServices() const
 {
+    EV << "MecPlatformManagerDyn::getAvailableMecServices" << endl;
     if(serviceRegistry == nullptr)
         return nullptr;
     else
@@ -101,6 +150,7 @@ const std::vector<ServiceInfo>* MecPlatformManagerDyn::getAvailableMecServices()
 
 const std::set<std::string>* MecPlatformManagerDyn::getAvailableOmnetServices() const
 {
+    EV << "MecPlatformManagerDyn::getAvailableOmnetServices" << endl;
     if(serviceRegistry == nullptr)
         return nullptr;
     else
